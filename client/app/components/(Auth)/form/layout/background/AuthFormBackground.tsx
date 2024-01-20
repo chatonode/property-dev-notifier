414de82ab5fe72b46d3d507085c59c75dd270dea
unknown <chatonode@gmail.com>
2024-01-20 20:39:41 +0000

    Added better clip-path to the AuthFormBackground with some animation like observing through the ocean. Fixed box shadow of AuthFormAvatar. And some more...

--- client/app/components/(Auth)/form/layout/background/AuthFormBackground.tsx
+++ client/app/components/(Auth)/form/layout/background/AuthFormBackground.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 import classes from './AuthFormBackground.module.css'
 
 import backgroundImage from '@/public/assets/images/forms/background/_40bc28f7-891a-4b01-85c3-7e60d5820a3c.jpg'
+// import backgroundImage from '@/public/assets/images/forms/background/_93281976-0b9f-4cd7-bd52-95399fb4bf5f.jpg'
+
 
 import ImageSpinner from '@/app/components/UI/SVG/Loading/ImageSpinner'
 
@@ -15,7 +17,11 @@ type TAuthFormBackgroundProps = {
 
 const AuthFormBackground = (props: TAuthFormBackgroundProps) => {
   return (
-    <div className={classes['background-container']}>
+    <div
+      className={`${classes['background-container']}${
+        props.isSubmitSuccessful ? ` ${classes.connecting}` : ''
+      }`}
+    >
       <div
         className={`${classes.background}${
           props.isSubmitSuccessful ? ` ${classes.connecting}` : ''
