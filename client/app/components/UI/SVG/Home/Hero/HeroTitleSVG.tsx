835a5e5a0da2937d23bfb2309816173ccea9b8db
unknown <chatonode@gmail.com>
2024-02-12 19:40:45 +0000

    Minimal style changes at HeroTitleSVG

--- client/app/components/UI/SVG/Home/Hero/HeroTitleSVG.tsx
+++ client/app/components/UI/SVG/Home/Hero/HeroTitleSVG.tsx
@@ -1,6 +1,9 @@
+import AnimatedGradientStops from '../../gradient/linear/AnimatedGradientStops'
 import classes from './HeroTitleSVG.module.css'
 
 const HeroTitleSVG = () => {
+  // const gradientId = `gradient-hero-title`
+
   return (
     <svg
       viewBox="0 0 240 80"
@@ -10,14 +13,14 @@ const HeroTitleSVG = () => {
       <text x="20" y="35" className={classes.small}>
         Notify
       </text>
-      <text x="130" y="65" className={classes.heavy}>
-        now
-      </text>
       <text x="55" y="35" className={classes.Rrrrr}>
         Property
       </text>
       <text x="55" y="55" className={classes.small}>
         Developers
+      </text>
+      <text x="130" y="65" className={classes.heavy}>
+        now!
       </text>
     </svg>
   )
