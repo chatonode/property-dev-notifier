3f91dcca97cbc19341ec3bfad5238b083b4c3a08
unknown <chatonode@gmail.com>
2024-03-05 06:03:52 +0000

    Added transition animation with some delay, but not seeming better since other elements prevent lex: 1 0 auto also has other elements in the list with lex: 0 0 var(---item-flex-basis)

--- client/app/components/(Users)/PropertyDevelopers/CRUD/list/PropertyDeveloperItem.tsx
+++ client/app/components/(Users)/PropertyDevelopers/CRUD/list/PropertyDeveloperItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { memo, useCallback, useState } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
 import { ERoute } from '@/app/types/enums'
@@ -20,18 +20,30 @@ const PropertyDeveloperItem = ({
 }: // onClick,
 TPropertyDeveloperItemProps) => {
   const router = useRouter()
+  const [onTransition, setOnTransition] = useState<boolean>(false)
 
   const itemClassNames = `${classes.item} ${
     isExpanded ? ` ${classes.expanded}` : ''
-  }`
+  }${onTransition ? ` ${classes.transitioning}` : ''}`
 
   const toggleItemHandler = useCallback(() => {
-    if (isExpanded) {
-      router.push(ERoute.PropertyDevelopers)
-    } else {
-      router.push(`${ERoute.PropertyDevelopers}/${developer.id}`)
-    }
+    setOnTransition(true)
   }, [])
+
+  useEffect(() => {
+    if (onTransition) {
+      const transitionInterval = setTimeout(() => {
+        // setOnTransition(false)
+
+        if (isExpanded) {
+          router.push(ERoute.PropertyDevelopers)
+        } else {
+          router.push(`${ERoute.PropertyDevelopers}/${developer.id}`)
+        }
+      }, 260)
+      return () => clearTimeout(transitionInterval)
+    }
+  }, [onTransition])
 
   return (
     <div className={itemClassNames} onClick={toggleItemHandler}>
